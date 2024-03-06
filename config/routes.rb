6594be003d237a07d04987700e1bebfbe6bff2e8
Hoang Ha <SmithMerch2301@gmail.com>
2024-03-06 14:45:34 +0700

    Add Show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "posts#index"
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
 end
